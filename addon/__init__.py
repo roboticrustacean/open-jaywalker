0f4f1219961f7b8c191ff1ebb941ad5b851e4f5c
roboticrustacean <uygarapan@gmail.com>
2026-06-03 00:12:18 +0300

    feat(addon): Open Jaywalker panel, run + build operators, state (#43)

---
+++ addon/__init__.py
@@ -0,0 +1,47 @@
+bl_info = {
+    "name": "Open Jaywalker",
+    "author": "Open Jaywalker",
+    "version": (0, 1, 0),
+    "blender": (3, 0, 0),
+    "location": "View3D > Sidebar > Open Jaywalker",
+    "description": "Convert humanoid rigs into ASAM OpenMATERIAL-compliant humans.",
+    "category": "Rigging",
+}
+
+import os
+import sys
+
+# When installed, the bundled pipeline packages sit beside this file. Put our own
+# directory on sys.path so their absolute imports (phase3_classifier.*, etc.) resolve.
+_ADDON_DIR = os.path.dirname(os.path.abspath(__file__))
+if _ADDON_DIR not in sys.path:
+    sys.path.append(_ADDON_DIR)
+
+import bpy
+
+from . import state
+from . import operators
+from . import ui
+
+_CLASSES = (
+    state.OJSettings,
+    operators.OJ_OT_run_pipeline,
+    operators.OJ_OT_build,
+    ui.OJ_PT_panel,
+)
+
+
+def register():
+    for cls in _CLASSES:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.open_jaywalker = bpy.props.PointerProperty(type=state.OJSettings)
+
+
+def unregister():
+    del bpy.types.Scene.open_jaywalker
+    for cls in reversed(_CLASSES):
+        bpy.utils.unregister_class(cls)
+
+
+if __name__ == "__main__":
+    register()
---
+++ addon/state.py
@@ -0,0 +1,14 @@
+"""Add-on state stored on the Scene (between Run pipeline and Build)."""
+
+import bpy
+
+
+class OJSettings(bpy.types.PropertyGroup):
+    has_plan: bpy.props.BoolProperty(default=False)
+    asset_dir: bpy.props.StringProperty(default="")
+    recommended_armature: bpy.props.StringProperty(default="")
+    mapped: bpy.props.IntProperty(default=0)
+    total: bpy.props.IntProperty(default=28)
+    missing_csv: bpy.props.StringProperty(default="")
+    is_crowd: bpy.props.BoolProperty(default=False)
+    character_count: bpy.props.IntProperty(default=0)
